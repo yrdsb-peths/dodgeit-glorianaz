01735ce26cfc5319ee744300d54cf2074d8bb137
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-09-12 13:13:22 -0400

    Add Fries

--- Hero.java
+++ Hero.java
@@ -12,9 +12,9 @@ public class Hero extends Actor
      * Act - do whatever the Hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    boolean atTop = true;
     public void act()
     {
-        boolean atTop = true;
         
         if(Greenfoot.mouseClicked(null))
         {
--- MyWorld.java
+++ MyWorld.java
@@ -20,5 +20,8 @@ public class MyWorld extends World
         
         Hero hero = new Hero();
         addObject(hero, 100, 100);
+        
+        Fries fries = new Fries();
+        addObject(fries, 600, 100);
     }
 }
